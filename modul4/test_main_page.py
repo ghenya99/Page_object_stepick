34ea6662d256f7089488bb4c58a9efae0cb68df4
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-02 17:11:29 +0300

    Добавление теста перехода на другую страницу

--- modul4/test_main_page.py
+++ modul4/test_main_page.py
@@ -1,5 +1,6 @@
-from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
+from .pages.locators import MainPageLocators
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -7,10 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-link = "http://selenium1py.pythonanywhere.com/"
 
 def go_to_login_page(browser):
-    link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    link = browser.find_element(*MainPageLocators.LOGIN_LINK)
     link.click()
 
 def test_guest_should_see_login_link(browser):
@@ -18,3 +18,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
