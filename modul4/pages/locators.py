1a4ca0a566e6b7199d1bed790cddd4e89a67a6f2
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-03 16:14:35 +0300

    Добавление локаторов:наименование продукта, цена, сообщения об успехе и тд.

--- modul4/pages/locators.py
+++ modul4/pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTR_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner")
+    MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "div.alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, "div.alert-info p strong")
