cb9dbaebd641c51c840c32691e5c95430365005e
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-04 11:05:19 +0300

    добавление теста перехода с главной страницы в корзину

--- modul4/test_main_page.py
+++ modul4/test_main_page.py
@@ -1,6 +1,6 @@
 from pages.login_page import LoginPage
 from .pages.main_page import MainPage
-from .pages.locators import MainPageLocators
+from .pages.basket_page import BasketPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -8,10 +8,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-
-def go_to_login_page(browser):
-    link = browser.find_element(*MainPageLocators.LOGIN_LINK)
-    link.click()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -26,3 +22,11 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_message_basket()
