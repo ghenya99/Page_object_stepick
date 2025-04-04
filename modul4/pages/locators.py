b426043cb1959c32657a1a10d720a56984f171e7
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-04 14:21:43 +0300

    добавление локаторов для создания пользователя

--- modul4/pages/locators.py
+++ modul4/pages/locators.py
@@ -7,6 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTR_FORM = (By.CSS_SELECTOR, "#register_form")
+    EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REDISTR_BUTTON = (By.CSS_SELECTOR, "button[name='registration_submit']")
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form")
@@ -20,6 +24,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BUTTON = (By.CSS_SELECTOR, "span.btn-group a.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     BASKET_ITEMS = (By.CSS_SELECTOR, "div.basket-title.hidden-xs")
