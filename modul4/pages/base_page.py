df43bcb3d0dc1597d0b971b3698da18cb2d14702
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-04 11:03:55 +0300

    добавление методов для страницы с корзиной

--- modul4/pages/base_page.py
+++ modul4/pages/base_page.py
@@ -4,15 +4,27 @@ import math
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-       # self.browser.implicitly_wait(timeout)
+
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_BUTTON)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
