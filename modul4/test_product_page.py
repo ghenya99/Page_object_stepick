64d9c183d1432bcebb4d37786bbce15b41e66b6d
Ghenya Yakshin <ghenya_yakshin99@mail.ru>
2025-04-04 14:20:25 +0300

    добавление тестов на создание юзера. Объединение тестов в класс. Добавление класса в фикстуру

--- modul4/test_product_page.py
+++ modul4/test_product_page.py
@@ -1,6 +1,10 @@
 import pytest
-from pages.product_page import ProductPage
+from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import time
+
+
 
 
 
@@ -16,6 +20,7 @@ from .pages.basket_page import BasketPage
                                       marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -48,6 +53,8 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.add_product_to_basket()
     page.should_disappear_success_message()
 
+
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -67,3 +74,37 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_message_basket()
+
+@pytest.mark.user
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = "SuperSecretPassword!"
+        # Открываем страницу регистрации
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+
+        # Регистрируем нового пользователя
+        page.register_new_user(email, password)
+
+        # Проверяем, что пользователь залогинен
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_basket_button()
+        page.add_product_to_basket()
+        page.should_be_success_message()
+        page.should_be_correct_product_name()
+        page.should_be_correct_price()
